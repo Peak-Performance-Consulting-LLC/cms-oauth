40f35f7177d42e7d7f89e611ad170315cd52878f
PPCONSULTING25 <Connect@ppconsultings.com>
2025-08-25 17:49:34 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,36 +1,50 @@
-const generateScript = require('./login_script.js')
+// OAuth "callback" endpoint (root handler)
+const { AuthorizationCode } = require('simple-oauth2');
 
-module.exports = (oauth2, oauthProvider) => {
-  function callbackMiddleWare (req, res, next) {
-    const code = req.query.code
-    var options = {
-      code: code
-    }
+const config = {
+  client: {
+    id: process.env.OAUTH_CLIENT_ID || process.env.CLIENT_ID,
+    secret: process.env.OAUTH_CLIENT_SECRET || process.env.CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  },
+};
 
-    if (oauthProvider === 'gitlab') {
-      options.client_id = process.env.OAUTH_CLIENT_ID
-      options.client_secret = process.env.OAUTH_CLIENT_SECRET
-      options.grant_type = 'authorization_code'
-      options.redirect_uri = process.env.REDIRECT_URL
-    }
+const client = new AuthorizationCode(config);
 
-    oauth2.getToken(options)
-      .then(result => {
-        const token = oauth2.createToken(result)
-        content = {
-          token: token.token.token.access_token,
-          provider: oauthProvider
-        }
-        return { message: 'success', content }
-      })
-      .catch(error => {
-        console.error('Access Token Error', error.message)
-        return { message: 'error', content: JSON.stringify(error) }
-      })
-      .then(result => {
-        const script = generateScript(oauthProvider, result.message, result.content)
-        return res.send(script)
-      })
+module.exports = async (req, res) => {
+  try {
+    const url = new URL(req.url, `https://${req.headers.host}`);
+    const code  = url.searchParams.get('code');
+    const state = url.searchParams.get('state') || '';
+
+    const base = process.env.BASE_URL || `https://${req.headers.host}`;
+    const redirectUri = `${base}/api/callback`;
+
+    const tokenParams = { code, redirect_uri: redirectUri };
+
+    const result = await client.getToken(tokenParams);
+    const token  = result.token; // { access_token, token_type, scope, ... }
+
+    // Post token back to CMS opener window (Decap listens for this)
+    const html = `
+      <html><body>
+        <script>
+          (function() {
+            const data = ${JSON.stringify({ token, provider: 'github', state })};
+            if (window.opener) window.opener.postMessage(data, '*');
+            window.close();
+          })();
+        </script>
+      </body></html>`;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(html);
+  } catch (err) {
+    console.error('callback error', err);
+    res.statusCode = 500;
+    res.end('OAuth callback error');
   }
-  return callbackMiddleWare
-}
+};
